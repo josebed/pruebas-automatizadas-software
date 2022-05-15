309a932534efb3378eb5db79ed568e4f726bd2d9
josebed <je.bedoya@uniandes.edu.co>
2022-05-15 16:26:56 -0500

    adding report

--- resemble/index.js
+++ resemble/index.js
@@ -1,51 +1,55 @@
-const playwright = require('playwright');
 const compareImages = require("resemblejs/compareImages")
 const config = require("./config.json");
 const fs = require('fs');
 
-const { viewportHeight, viewportWidth, browsers, options } = config;
+const { viewportHeight, viewportWidth, options } = config;
 
 async function executeTest(){
-    if(browsers.length === 0){
-        return;
-    }
+    
     let resultInfo = {}
     let datetime = new Date().toISOString().replace(/:/g,".");
-    for(b of browsers){
-        if(!b in ['chromium', 'webkit', 'firefox']){
-            return;
-        }
-        if (!fs.existsSync(`./results/${datetime}`)){
-            fs.mkdirSync(`./results/${datetime}`, { recursive: true });
-        }
-        //Launch the current browser context
-        const browser = await playwright[b].launch({headless: true, viewport: {width:viewportWidth, height:viewportHeight}});
-        const context = await browser.newContext();
-        const page = await context.newPage();
-        await page.goto(config.url);
-        await page.screenshot({ path: `./results/${datetime}/before-${b}.png` });
-        await page.click('#generate');
-        await page.screenshot({ path: `./results/${datetime}/after-${b}.png` });
-        await browser.close();
+    krakenPath = '../kraken/artifacts/';
+    puppeteerPAth = '../puppeteer/artifacts';
 
-        const data = await compareImages(
-            fs.readFileSync(`./results/${datetime}/before-${b}.png`),
-            fs.readFileSync(`./results/${datetime}/after-${b}.png`),
-            options
-        );
-        resultInfo[b] = {
-            isSameDimensions: data.isSameDimensions,
-            dimensionDifference: data.dimensionDifference,
-            rawMisMatchPercentage: data.rawMisMatchPercentage,
-            misMatchPercentage: data.misMatchPercentage,
-            diffBounds: data.diffBounds,
-            analysisTime: data.analysisTime
-        }
-        fs.writeFileSync(`./results/${datetime}/compare-${b}.png`, data.getBuffer());
+    
+    let krakenFolders= ['scenario3', 'scenario4', 'scenario5', 'scenario15', 'scenario20'];
+    let index = 0;
 
-    }
-    fs.writeFileSync(`./results/${datetime}/report.html`, createReport(datetime, resultInfo));
-    fs.copyFileSync('./index.css', `./results/${datetime}/index.css`);
+    for (let folder of krakenFolders){
+        console.log(krakenPath +'v1/'+ folder);  
+        fs.readdir(krakenPath +'v1/'+ folder, function (err, files) {
+            if (err) {
+                console.log("Error");
+                return;
+            }
+            console.log("files");   
+            return files;
+        });
+
+        for (let file of files){
+            const data = await compareImages(
+                fs.readFileSync(krakenPath+'v1/'+folder+file),
+                fs.readFileSync(krakenPath+'v2/'+folder+file),
+                options
+            ); 
+
+            resultInfo[index] = {
+                isSameDimensions: data.isSameDimensions,
+                dimensionDifference: data.dimensionDifference,
+                rawMisMatchPercentage: data.rawMisMatchPercentage,
+                misMatchPercentage: data.misMatchPercentage,
+                diffBounds: data.diffBounds,
+                analysisTime: data.analysisTime
+            }
+            fs.writeFileSync(`./comparacion/kraken/compare-v1v2-`+folder+file+`.png`, data.getBuffer());
+            index++;
+
+        }
+
+    }        
+        
+    fs.writeFileSync(`./comparacion/kraken/report.html`, createReport(datetime, resultInfo));
+    fs.copyFileSync('./index.css', `./comparacion/kraken/index.css`);
 
     console.log('------------------------------------------------------------------------------------')
     console.log("Execution finished. Check the report under the results folder")
@@ -54,26 +58,26 @@ async function executeTest(){
 (async ()=>console.log(await executeTest()))();
 
 
-function browser(b, info){
+function browser(info){
     return `<div class=" browser" id="test0">
     <div class=" btitle">
-        <h2>Browser: ${b}</h2>
+        <h2>Scenario 20</h2>
         <p>Data: ${JSON.stringify(info)}</p>
     </div>
     <div class="imgline">
       <div class="imgcontainer">
         <span class="imgname">Reference</span>
-        <img class="img2" src="before-${b}.png" id="refImage" label="Reference">
+        <img class="img2" src="../../../kraken/artifacts/v1/scenario20/login.png" id="refImage" label="Reference">
       </div>
       <div class="imgcontainer">
         <span class="imgname">Test</span>
-        <img class="img2" src="after-${b}.png" id="testImage" label="Test">
+        <img class="img2" src="../../../kraken/artifacts/v2/scenario20/login.png" id="testImage" label="Test">
       </div>
     </div>
     <div class="imgline">
       <div class="imgcontainer">
         <span class="imgname">Diff</span>
-        <img class="imgfull" src="./compare-${b}.png" id="diffImage" label="Diff">
+        <img class="imgfull" src="compare-v1v2-sc20.png" id="diffImage" label="Diff">
       </div>
     </div>
   </div>`
@@ -92,7 +96,7 @@ function createReport(datetime, resInfo){
             </h1>
             <p>Executed: ${datetime}</p>
             <div id="visualizer">
-                ${config.browsers.map(b=>browser(b, resInfo[b]))}
+                ${browser(resInfo)}
             </div>
         </body>
     </html>`
